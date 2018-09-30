8571cc5d82ab524435827420389f16759005df24
Natalya Rohozhynska <rohozhinska@gmail.com>
2018-09-30 17:05:03 +0000

    sd

--- js/index.js
+++ js/index.js
@@ -1,23 +1,25 @@
 const aNum = prompt('Enter a');
 const bNum = prompt('Enter b');
 const cNum = prompt('Enter c');
- let descr = bNum * bNum - 4 * aNum * cNum;
- const xSingle = ((-bNum + Math.sqrt(descr)) / (2 * aNum));
- const xFirst = ((-bNum + Math.sqrt(descr)) / (2 * aNum));
- const xSecond = ((-bNum - Math.sqrt(descr)) / (2 * aNum));
- function solvEq(bNum, descr, aNum, xFirst, xSecond, xSingle) {
-  if (descr == 0)
-  {
-   alert(xSingle);
-  }
-  else if (descr < 0) {
-   alert('Equation doesn`t have solution');
-  }
-  else  {
-   alert('x1 = ' + xFirst + ' x2 = ' + xSecond);
-  }
+
+function findingDescr(aNum, bNum, cNum) {
+ return bNum * bNum - 4 * aNum * cNum
+}
+let descr = findingDescr(aNum, bNum, cNum)
+
+function solvEq(bNum, descr, aNum) {
+ if (descr == 0) {
+  const xSingle = ((-bNum + Math.sqrt(descr)) / (2 * aNum));
+  return xSingle;
  }
-solvEq(bNum, descr, aNum, xSingle, xFirst, xSecond);
-
-
-
+ else if (descr < 0) {
+  return 'Equation doesn`t have solution';
+ }
+ else {
+  const xFirst = ((-bNum + Math.sqrt(descr)) / (2 * aNum));
+  const xSecond = ((-bNum - Math.sqrt(descr)) / (2 * aNum));
+  return 'x1 = ' + xFirst + ' x2 = ' + xSecond;
+ }
+}
+let res = solvEq(bNum, descr, aNum);
+alert(res);
